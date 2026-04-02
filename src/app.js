9d081a5c2b2b4a2b90e8840cb34867a3be62b91b
DarioN370 <netobis2015@gmail.com>
2026-04-02 13:08:06 -0300

    feat: começando e terminando a criacao do banco de dados e conectando ele usando variaveis de ambiente

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@
 const express = require('express')
 // instalar o express em uma const APP
 const app = express()
+//requerendo a conexao do banco
+const conn = require('./database/conn.js')
 
 //exportando a lib
 module.exports = app
---
+++ src/database/conn.js
@@ -0,0 +1,27 @@
+//requerer as variaveis de ambiente - DOTENV
+require('dotenv').config()
+
+// requerer a classe sequelize
+const {Sequelize} = require('sequelize')
+
+//conecta com o banco
+const conn = new Sequelize(
+    process.env.DATABASE, 
+    process.env.DB_USER, 
+    process.env.DB_PASS,
+    {
+    host: process.env.DB_HOST,
+    dialect: "mysql"
+    }
+)
+
+// autenticacao do banco com try catch para tratar possiveis erros
+try {
+    conn.authenticate()
+    console.log("Banco de dados Conectado")
+} catch (error) {
+    console.log(`Banco não inicializado: ${error}`)
+}
+
+//exportacao do banco
+module.exports = conn
